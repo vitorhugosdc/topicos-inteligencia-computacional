5f90277d5f936f9a439426d50f2354939fc41f3e
Vitor <vitorvitinho9900@hotmail.com>
2024-03-08 12:21:57 -0300

    mod

--- modelo.py
+++ modelo.py
@@ -1,27 +1,20 @@
+import csv
 import os
-import numpy as np
-from keras.models import load_model
 from keras.preprocessing.image import img_to_array, load_img
+from keras.models import load_model
 from keras.applications.vgg16 import preprocess_input
-from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
 
-# Carregar o modelo treinado
+# Carregar o modelo
 model = load_model('sword_classifier_model.keras')
 
-# Obter o mapeamento de índices de classe para nomes de classe do gerador
-base_dir = os.path.dirname(os.path.realpath(__file__))
-train_dir = os.path.join(base_dir, 'treino')
-train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
-train_generator = train_datagen.flow_from_directory(
-    train_dir,
-    target_size=(224, 224),
-    batch_size=32,
-    class_mode='categorical'
-)
+# Diretório com as imagens para testar
+test_images_dir = './testes_modelo'
 
-class_labels = dict((v,k) for k,v in train_generator.class_indices.items())
+# mapeamento de índices de classe para nomes de classe
+class_labels = {0: 'colossal_swords', 1: 'dagger', 2: 'straight_sword'}
 
-# Preparar a imagem para predição
+# Preparar uma imagem
 def prepare_image(image_path):
     image = load_img(image_path, target_size=(224, 224))
     image = img_to_array(image)
@@ -29,13 +22,20 @@ def prepare_image(image_path):
     image = preprocess_input(image)
     return image
 
-image_path = './straight_sword/teste/warhawks_talon_straight_sword.png'
-image = prepare_image(image_path)
+results_file = 'test_results.csv'
+with open(results_file, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Image Name', 'Predicted Class', 'Probability'])
 
-# Fazer a predição
-predictions = model.predict(image)
-predicted_class_index = np.argmax(predictions, axis=1)
-predicted_class_name = class_labels[predicted_class_index[0]]
-predicted_probability = np.max(predictions)
+    # Iterar sobre todas as imagens no diretório de teste
+    for image_name in os.listdir(test_images_dir):
+        if image_name.lower().endswith(('.png', '.jpg', '.jpeg')):
+            image_path = os.path.join(test_images_dir, image_name)
+            image = prepare_image(image_path)
+            predictions = model.predict(image)
+            predicted_class_index = np.argmax(predictions, axis=1)[0]
+            predicted_class_name = class_labels[predicted_class_index]
+            predicted_probability = np.max(predictions)
+            writer.writerow([image_name, predicted_class_name, f"{predicted_probability:.4f}"])
 
-print(f"Classe Predita: {predicted_class_name}, Probabilidade: {predicted_probability:.4f}")
+print(f"Results saved to {results_file}")
